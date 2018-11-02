3480d22980c21f8a9c5ffc9d9ce4835c2c6f5299
heenakaur13 <43087253+heenakaur13@users.noreply.github.com>
2018-11-02 20:00:38 +0530

    Add files via upload

---
+++ greater no.py
@@ -0,0 +1,9 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> a=int(input("enter the first no"))
+enter the first no5
+>>> b=int(input("enter the second no"))
+enter the second no10
+>>> print("the greater no is",b)
+the greater no is 10
+>>> 
